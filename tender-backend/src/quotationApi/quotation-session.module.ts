89bab999c0b7b7cac3f56fb6b181287dd3d8e717
Oleg <olegbelii4311@gmail.com>
2024-11-09 14:10:39 +0300

    fix service

--- tender-backend/src/quotationApi/quotation-session.module.ts
+++ tender-backend/src/quotationApi/quotation-session.module.ts
@@ -3,9 +3,12 @@
 import { Module } from "@nestjs/common";
 import { HttpModule } from "@nestjs/axios";
 import { QuotationSessionController } from "./quotation-session.controller";
+import { QuotationSessionService } from "./quotation-session.service";
 
 @Module({
 	imports: [HttpModule],
 	controllers: [QuotationSessionController],
+	providers: [QuotationSessionService],
+	exports: [QuotationSessionService],
 })
 export class QuotationSessionModule {}
