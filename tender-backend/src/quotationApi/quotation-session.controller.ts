d4fad6bdeb766e2483af285ecb881b97703d9b95
Oleg <olegbelii4311@gmail.com>
2024-11-09 14:07:26 +0300

    use script in back

--- tender-backend/src/quotationApi/quotation-session.controller.ts
+++ tender-backend/src/quotationApi/quotation-session.controller.ts
@@ -1,68 +1,17 @@
 // quotation-session.controller.ts
 
-import {
-	Controller,
-	Post,
-	Body,
-	HttpException,
-	HttpStatus,
-} from "@nestjs/common";
-import { HttpService } from "@nestjs/axios";
-import { lastValueFrom } from "rxjs";
+import { Controller, Post, Body } from "@nestjs/common";
+import { QuotationSessionService } from "./quotation-session.service";
 
 @Controller("quotation-sessions")
 export class QuotationSessionController {
-	constructor(private readonly httpService: HttpService) {}
+	constructor(
+		private readonly quotationSessionService: QuotationSessionService
+	) {}
 
-	@Post("check_title")
-	async checkTitle(@Body() data: { title: string }) {
-		try {
-			const response = await lastValueFrom(
-				this.httpService.post("http://127.0.0.1:5300/api/check_title", {
-					title: data.title,
-				})
-			);
-			return response.data;
-		} catch (error) {
-			throw new HttpException(
-				error?.response?.data || "Ошибка при проверке наименования",
-				HttpStatus.BAD_GATEWAY
-			);
-		}
-	}
-
-	@Post("check_contract_enforced")
-	async checkContractEnforced(@Body() data: { contractEnforced: string }) {
-		try {
-			const response = await lastValueFrom(
-				this.httpService.post(
-					"http://127.0.0.1:5300/api/check_contract_enforced",
-					{ contractEnforced: data.contractEnforced }
-				)
-			);
-			return response.data;
-		} catch (error) {
-			throw new HttpException(
-				error?.response?.data || "Ошибка при проверке обеспечения контракта",
-				HttpStatus.BAD_GATEWAY
-			);
-		}
-	}
-
-	@Post("check_photo")
-	async checkPhoto(@Body() data: { photoUrl: string }) {
-		try {
-			const response = await lastValueFrom(
-				this.httpService.post("http://127.0.0.1:5300/api/check_photo", {
-					photoUrl: data.photoUrl,
-				})
-			);
-			return response.data;
-		} catch (error) {
-			throw new HttpException(
-				error?.response?.data || "Ошибка при проверке фото",
-				HttpStatus.BAD_GATEWAY
-			);
-		}
+	@Post("check")
+	async checkData(@Body() data: any) {
+		// Вызов сервиса для обработки данных и отправки их на внешние API
+		return await this.quotationSessionService.processAndSendData(data);
 	}
 }
--- tender-backend/src/quotationApi/quotation-session.service.ts
+++ tender-backend/src/quotationApi/quotation-session.service.ts
@@ -1,19 +1,52 @@
 // quotation-session.service.ts
 
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { QuotationSession } from "./quotation-session.entity";
+import { Injectable, HttpException, HttpStatus } from "@nestjs/common";
+import { HttpService } from "@nestjs/axios";
+import { lastValueFrom } from "rxjs";
+import { parseQuotationSession } from "src/parser/parseQuotationSession";
 
 @Injectable()
 export class QuotationSessionService {
-	constructor(
-		@InjectRepository(QuotationSession)
-		private readonly quotationRepository: Repository<QuotationSession>
-	) {}
+	constructor(private readonly httpService: HttpService) {}
 
-	async create(data: Partial<QuotationSession>): Promise<QuotationSession> {
-		const quotationSession = this.quotationRepository.create(data);
-		return this.quotationRepository.save(quotationSession);
+	async processAndSendData(data: any): Promise<any> {
+		try {
+			// 1. Обработка данных с помощью скрипта
+			const parsedData = await parseQuotationSession(data);
+
+			// 2. Отправка обработанных данных на внешние API параллельно
+			const [titleResponse, contractResponse, photoResponse] =
+				await Promise.all([
+					lastValueFrom(
+						this.httpService.post("http://127.0.0.1:5300/api/check_title", {
+							title: parsedData.title,
+						})
+					),
+					lastValueFrom(
+						this.httpService.post(
+							"http://127.0.0.1:5300/api/check_contract_enforced",
+							{ contractEnforced: parsedData.contractEnforced }
+						)
+					),
+					lastValueFrom(
+						this.httpService.post("http://127.0.0.1:5300/api/check_photo", {
+							photoUrl: parsedData.specifications[0].image,
+						})
+					),
+				]);
+
+			// Возвращаем результаты всех проверок
+			return {
+				titleCheck: titleResponse.data,
+				contractCheck: contractResponse.data,
+				photoCheck: photoResponse.data,
+			};
+		} catch (error) {
+			throw new HttpException(
+				error?.response?.data ||
+					"Ошибка при обработке данных и отправке на проверку",
+				HttpStatus.BAD_GATEWAY
+			);
+		}
 	}
 }
